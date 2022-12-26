14e631edbca38aee3a2ec7117541b0aedce24144
Adhil Salim <bizadhil@gmail.com>
2022-12-26 20:38:07 +0530

    replaced banner with json

--- js/jparse.js
+++ js/jparse.js
@@ -1,10 +1,12 @@
 import mini from '../json/mini.json' assert { type: 'json' };
 import insta from '../json/insta.json' assert { type: 'json' };
+import banner from '../json/banner.json' assert { type: 'json' };
 
 //------------------mini------------------//
 //1
 var html = "";
 var prehtml = `<div class="section-title"><h4>Hot Trend</h4></div>`;
+var posthtml = "";
 
 mini.ht.forEach((item) => {
     html += `<div class="trend__item">
@@ -15,7 +17,7 @@ mini.ht.forEach((item) => {
         <h6>${item.name}</h6>
         <div class="product__price">$ ${item.price}</div>
     </div>
-    </div>`
+    </div>`;
 });
 document.getElementById('mini-s-1').innerHTML = prehtml + html;
 
@@ -32,7 +34,7 @@ mini.bs.forEach((item) => {
         <h6>${item.name}</h6>
         <div class="product__price">$ ${item.price}</div>
     </div>
-    </div>`
+    </div>`;
 });
 document.getElementById('mini-s-2').innerHTML = prehtml + html;
 
@@ -49,7 +51,7 @@ mini.bs.forEach((item) => {
         <h6>${item.name}</h6>
         <div class="product__price">$ ${item.price}</div>
     </div>
-    </div>`
+    </div>`;
 });
 document.getElementById('mini-s-3').innerHTML = prehtml + html;
 
@@ -66,6 +68,30 @@ insta.posts.forEach((item) => {
             <a href="${item.post_url}">${item.user_id}</a>
         </div>
     </div>
-</div>`
+</div>`;
 });
 document.getElementById('insta-s-0').innerHTML = html;
+
+//------------------banner------------------//
+//1
+html = "";
+prehtml = `<section class="banner set-bg" data-setbg="${banner.image}">
+<div class="container">
+    <div class="row">
+        <div class="col-xl-7 col-lg-8 m-auto">
+            <div class="banner__slider owl-carousel" id="banner-s-1">`;
+posthtml = `</div></div></div></div></section>`;
+
+banner.slides.forEach((item) => {
+    html +=
+        `<div class="banner__item">
+        <div class="banner__text">
+            <span>${item.small_text}</span>
+            <h1>${item.big_text}</h1>
+            <a href="${item.button_link}">${item.button_text}</a>
+        </div>
+    </div>`;
+});
+
+console.log(prehtml + html + posthtml)
+document.getElementById("banner-s-0").insertAdjacentHTML("afterend", prehtml + html + posthtml);
