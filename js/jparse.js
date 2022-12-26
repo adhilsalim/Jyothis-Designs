403677d8e96a7376923428d084e392fc8a4e95bd
Adhil Salim <bizadhil@gmail.com>
2022-12-26 19:09:57 +0530

    Replaced mini section with json

--- js/jparse.js
+++ js/jparse.js
@@ -1,12 +1,13 @@
-import hotTrendData from '../json/hotTrend.json' assert { type: 'json' };
+import mini from '../json/mini.json' assert { type: 'json' };
 
+//1
 var html = "";
 var prehtml = `<div class="section-title"><h4>Hot Trend</h4></div>`;
 
-hotTrendData.forEach((item) => {
+mini.ht.forEach((item) => {
     html += `<div class="trend__item">
     <div class="trend__item__pic">
-        <img src="${item.image}" alt="${item.name}">
+        <img src="${item.image}" alt="${item.name}" title="${item.product_id}">
     </div>
     <div class="trend__item__text">
         <h6>${item.name}</h6>
@@ -15,4 +16,37 @@ hotTrendData.forEach((item) => {
     </div>`
 });
 
-document.getElementById('check').innerHTML = prehtml + html;
+document.getElementById('mini-s-1').innerHTML = prehtml + html;
+//2
+html = "";
+prehtml = `<div class="section-title"><h4>BEST SELLER</h4></div>`;
+
+mini.bs.forEach((item) => {
+    html += `<div class="trend__item">
+    <div class="trend__item__pic">
+        <img src="${item.image}" alt="${item.name}" title="${item.product_id}">
+    </div>
+    <div class="trend__item__text">
+        <h6>${item.name}</h6>
+        <div class="product__price">$ ${item.price}</div>
+    </div>
+    </div>`
+});
+document.getElementById('mini-s-2').innerHTML = prehtml + html;
+
+//3
+html = "";
+prehtml = `<div class="section-title"><h4>FEATURE</h4></div>`;
+
+mini.bs.forEach((item) => {
+    html += `<div class="trend__item">
+    <div class="trend__item__pic">
+        <img src="${item.image}" alt="${item.name}" title="${item.product_id}">
+    </div>
+    <div class="trend__item__text">
+        <h6>${item.name}</h6>
+        <div class="product__price">$ ${item.price}</div>
+    </div>
+    </div>`
+});
+document.getElementById('mini-s-3').innerHTML = prehtml + html;
