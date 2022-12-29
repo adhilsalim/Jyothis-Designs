683a7590b8a02a7a1ade94a2df73e405943e0940
Adhil Salim <bizadhil@gmail.com>
2022-12-29 08:07:28 +0530

    UI update in size selector

---
+++ js/jparse-size.js
@@ -0,0 +1,27 @@
+import insta from '../json/insta.json' assert { type: 'json' };
+
+//------------------insta------------------//
+var html = "";
+var prehtml = "";
+var posthtml = "";
+
+insta.posts.forEach((item) => {
+    html +=
+        `<div class="col-lg-2 col-md-4 col-sm-4 p-0">
+    <div class="instagram__item set-bg" data-setbg=${item.image}>
+        <div class="instagram__text">
+            <i class="fa fa-instagram"></i>
+            <a href="${item.post_url}">${item.user_id}</a>
+        </div>
+    </div>
+</div>`;
+});
+
+document.getElementById('insta-s-0').innerHTML = html;
+
+
+//FUNCTIONS
+$('.set-bg').each(function () {
+    var bg = $(this).data('setbg');
+    $(this).css('background-image', 'url(' + bg + ')');
+});
